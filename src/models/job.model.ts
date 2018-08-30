beeb62a4ab0c8dc7437bca8a534c1eb7392ff4b2
serjio1905 <serjio.rv@gmail.com>
2018-08-30 11:58:38 +0200

    cv object fixed

--- src/models/job.model.ts
+++ src/models/job.model.ts
@@ -3,11 +3,13 @@ export class Job {
     public position: string;
     public date_from: string;
     public date_till: string;
-    public description: string
+    public description: string;
+    public frontend_percentage: number;
 
-    constructor (company: string, position: string, date_from?: string, date_till?: string, description?: string) {
+    constructor (company: string, position: string, frontend_percentage?: number, date_from?: string, date_till?: string, description?: string) {
         this.company = company;
         this.position = position;
+        this.frontend_percentage = frontend_percentage;
         this.date_from = date_from;
         this.date_till = date_till;
         this.description = description;
