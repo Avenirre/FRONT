be8f5e398094c97dee299879ae0380df85b92c69
DoroshenkoViktor <viktor@Viktors-MacBook-Air.local>
2018-09-04 19:19:48 +0300

    reg form reactive model

--- src/app/auth/company-reg-form/company-reg-form.component.ts
+++ src/app/auth/company-reg-form/company-reg-form.component.ts
@@ -1,9 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ModalService} from '../../modal/modal.service';
-import {Router} from '@angular/router';
 import {environment} from '../../../environments/environment';
-import {AuthService} from '../auth.service';
 import {Applicant} from '../../../models/auth/applicant.model';
+import {FormBuilder, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-company-reg-form',
@@ -14,29 +13,57 @@ import {Applicant} from '../../../models/auth/applicant.model';
   ]
 })
 export class CompanyRegFormComponent implements OnInit {
-  company: Applicant = new Applicant();
-  confirmPass: string;
+  regForm = this.fb.group({
+    companyDetails: this.fb.group({
+      companyName: [''],
+      country: [''],
+      website: [''],
+      cityTown: [''],
+      street: [''],
+      houseBuilding: [''],
+      postcode: [''],
+      phone: ['']
+    }),
+    applicantDetails: this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      position: [''],
+      email: [''],
+      username: ['', Validators.required],
+    }),
+    passwordGroup: this.fb.group({
+      password: ['', Validators.required],
+      confirmPass: ['', Validators.required],
+    })
+  });
+
+  applicant: Applicant = new Applicant();
   private routes = environment.routes;
 
   constructor(
+    private fb: FormBuilder,
     private modalService: ModalService,
-    private router: Router,
-    private authService: AuthService
   ) {
-    this.company.user_type = 'company';
   }
 
   ngOnInit() {
+    this.applicant.user_type = 'COMPANY';
   }
 
-  openCandidateModal(link: string) {
+  openCandidateModal() {
     this.modalService.openModal(this.routes['regCandidate']);
   }
 
   submitRegistration() {
+    console.log(this.regForm.value);
+    // this.assembleApplicant(form);
     // console.log(this.company);
-    this.modalService.closeModal();
-    this.authService.createApplicant(this.company);
+    // this.modalService.closeModal();
+    // this.authService.createApplicant(this.applicant);
+  }
+
+  private assembleApplicant(form: NgForm) {
+    // this.applicant.firstName =
   }
 }
 
