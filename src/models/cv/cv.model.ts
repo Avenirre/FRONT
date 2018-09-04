5f838010e3d5af57124295548b7e9fd50f9ca57b
serjio1905 <serjio.rv@gmail.com>
2018-09-04 15:41:25 +0200

    qqq

--- src/models/cv/cv.model.ts
+++ src/models/cv/cv.model.ts
@@ -8,42 +8,47 @@ import { Language } from './cv.lang.model';
 import { Template } from './cv.template.model';
 
 export class CV {
-    id: number;
-    first_name: string;
-    second_name: string;
-    profile_id: number;
-    summary: string;
-    about: string;
-    email: string;
-    phone: string;
-    residence: string;
-    birthday: string;
-    linkedin: string;
-    github: string;
-    recommendations: string;
-    title: string;
-    activated: boolean;
-    portfolio: string;
-    preferencedArea: string;
-    salaryFromPreference: number;
-    salaryTillPreference: number;
+    public id: number;
+    public first_name: string;
+    public second_name: string;
+    public profile_id: number;
+    public summary: string;
+    public about: string;
+    public email: string;
+    public phone: string;
+    public residence: string;
+    public birthday: string;
+    public linkedin: string;
+    public github: string;
+    public recommendations: string;
+    public title: string;
+    public activated: boolean;
+    public portfolio: string;
+    public preferencedArea: string;
+    public salaryFromPreference: number;
+    public salaryTillPreference: number;
 
-    position_preference: Position;
-    template: Template;
-    cvLang: [ Language ];
-    cvSkill: [ Skill ];
-    education: [ Education ];
-    cvJobs: [ Job ];
-    cvAchievements: [ Achievement ];
-    cvCertification: [ Certification ];
-    cvActivity: [ Activity ];
+    public position_preference: Position;
+    public template: Template;
+    public cvLang: [ Language ];
+    public cvSkill: [ Skill ];
+    public education: [ Education ];
+    public cvJobs: [ Job ];
+    public cvAchievements: [ Achievement ];
+    public cvCertification: [ Certification ];
+    public cvActivity: [ Activity ];
 
-    public constructor() {
-
+    public constructor(cv?) {
+        if (!cv) {
+            this.id = null;
+            this.profile_id = 0;
+        }
     }
 
     public compileActivity() {
+        for (let i = 0; i < this.cvJobs.length; i++) {
 
+        }
     }
 
     public unCompileActivity() {
