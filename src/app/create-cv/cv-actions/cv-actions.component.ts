0c1700e927903f0f84e839d0194c0ec7b3f7b0fb
serjio1905 <serjio.rv@gmail.com>
2018-09-11 20:09:46 +0200

    20181109_9

--- src/app/create-cv/cv-actions/cv-actions.component.ts
+++ src/app/create-cv/cv-actions/cv-actions.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CvService} from '../../../services/cv.service';
 import {CV} from '../../../models/cv/cv.model';
+import {ActivatedRoute} from '@angular/router';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-cv-actions',
@@ -11,7 +13,9 @@ export class CvActionsComponent implements OnInit {
   title: string;
   cv: CV;
   onlyShowMode = true;
-  constructor(private cvService: CvService) { }
+  url_share: string;
+  constructor(private cvService: CvService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.title = this.cvService.setCV().title;
@@ -20,6 +24,11 @@ export class CvActionsComponent implements OnInit {
       this.title = cv.title;
       this.onlyShowMode = this.cvService.onlyShowMode;
     });
+    this.route.params.subscribe(
+        (params) => {
+          this.url_share = environment.apiUrl + ':' + environment.apiPort + '/cv/' + params['id'];
+        }
+    );
   }
 
     activateCv() {
@@ -30,5 +39,9 @@ export class CvActionsComponent implements OnInit {
     saveCv() {
         this.cv = this.cvService.setCV();
         this.cvService.saveCV();
+    }
+
+    copyLink() {
+      console.log(this.url_share);
     }
 }
